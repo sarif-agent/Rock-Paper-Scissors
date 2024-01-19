d8c00280239e22999b2e620cca189befeb7ca685
Berkan K <berkan0hk@gmail.com>
2024-01-19 18:19:01 +0300

    rounds are done

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
 console.log("Hello World!");
 
-// let rock = "Rock beats Scissors, but loses to Paper";
-// let scissors = "Scissors beats Paper, but loses to Rock";
-// let paper = "Paper beats Rock, but loses to Scissors";
+let userPoint = 0;
+let computerPoint = 0;
 
-function getComputerChoice(a, b, c) {
+function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
 
+
   if (randomChoice === 0) {
-    return a;
+    return "rock";
   } else if (randomChoice === 1) {
-    return b;
+    return "paper";
   } else {
-    return c;
+    return "scissors";
   }
 }
 
-console.log(getComputerChoice("rock", "scissors", "paper"));
+
+
+function getPlayerChoice(a) {
+  let playerchoice = prompt("Rock, Paper Scissors?");
+  let casePlayerChoice = playerchoice.toLowerCase();
+
+  return casePlayerChoice;
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Ties";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    userPoint++
+    return "You Win! Rock beats the Scissors!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    userPoint++
+    return "You Win! Paper beats the Rock!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    userPoint++
+    return "You Win! Scissors beats the Papper!";
+  } else {
+    computerPoint++
+    return `You Lose! ${computerSelection} beats the ${playerSelection}`;
+  }
+}
+
+function game() {
+  computerPoint = 0;
+  userPoint = 0;
+  for (let game = 0; game < 5; game++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult)
+  }
+  if (userPoint > computerPoint) {
+    console.log(`You Win! Congratulations`)
+  } else if (computerPoint > userPoint) {
+    console.log("You Lose.")
+  } else {
+    console.log("Ties.")
+  }
+}
+
+game();
+
+
