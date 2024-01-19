9171deebefb329047520dd9da7c0d9aa0264ed84
Berkan K <berkan0hk@gmail.com>
2024-01-19 15:39:02 +0300

    trying out some functions? :d

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+// let rock = "Rock beats Scissors, but loses to Paper";
+// let scissors = "Scissors beats Paper, but loses to Rock";
+// let paper = "Paper beats Rock, but loses to Scissors";
+
+function getComputerChoice(a, b, c) {
+
+  return a, b, c
+}
+
+console.log(getComputerChoice(rock, scissors, paper));
