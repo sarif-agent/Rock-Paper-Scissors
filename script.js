6818ebfc56ae6d5eec4a7ab8210b3799d3457f8b
Berkan K <berkan0hk@gmail.com>
2024-02-26 08:56:31 +0300

    index HTML change, DOM UI create

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 console.log("Hello World!");
 
-const final = document.getElementById("final")
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const OutCome = document.querySelector('.outcome');
 
 
 let userPoint = 0;
@@ -8,7 +11,6 @@ let computerPoint = 0;
 
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
-
 
   if (randomChoice === 0) {
     return "rock";
@@ -20,57 +22,75 @@ function getComputerChoice() {
 }
 
 
-function getPlayerChoice(a) {
-  let playerchoice = prompt("Rock, Paper Scissors?");
-  let casePlayerChoice = playerchoice.toLowerCase();
+rock.addEventListener('click', () => {
+  const computerChoice = getComputerChoice();
+  const playerChoice = rock.innerText.toLowerCase();
+  console.log(playerChoice);
+  playRound(playerChoice, computerChoice);
+})
 
-  return casePlayerChoice;
-}
+paper.addEventListener('click', () => {
+  const computerChoice = getComputerChoice();
+  const playerChoice = paper.innerText.toLowerCase();
+  console.log(playerChoice);
+  playRound(playerChoice, computerChoice);
+})
 
+scissors.addEventListener('click', () => {
+  const computerChoice = getComputerChoice();
+  const playerChoice = scissors.innerText.toLowerCase();
+  console.log(playerChoice);
+  playRound(playerChoice, computerChoice);
+})
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "Ties";
+    const p = document.createElement('p');
+    p.innerText = `Ties ${playerSelection} to ${computerSelection}`;
+    OutCome.appendChild(p);
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     userPoint++
-    return `You Win! Rock beats the Scissors!`;
+    const p = document.createElement('p');
+    p.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+    OutCome.appendChild(p);
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     userPoint++
-    return "You Win! Paper beats the Rock!";
+    const p = document.createElement('p');
+    p.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+    OutCome.appendChild(p);
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     userPoint++
-    return "You Win! Scissors beats the Papper!";
+    const p = document.createElement('p');
+    p.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+    OutCome.appendChild(p);
   } else {
     computerPoint++
-    return `You Lose! ${computerSelection} beats the ${playerSelection}`;
+    const p = document.createElement('p');
+    p.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    OutCome.appendChild(p);
   }
 }
 
 
-function game() {
-  computerPoint = 0;
-  userPoint = 0;
-  for (let game = 0; game < 5; game++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const roundResult = playRound(playerSelection, computerSelection);
-    console.log(roundResult)
-  }
-  if (userPoint > computerPoint) {
-    final.innerText = `You Win! Your score is ${userPoint} against ${computerPoint}. Congratulations.`
-    console.log(`You Win! Your score is ${userPoint} against ${computerPoint}. Congratulations.`)
-  } else if (computerPoint > userPoint) {
-    final.innerText = `You Lose! Your score is ${userPoint} against ${computerPoint}. Bad Game.`
-    console.log(`You Lose! Your score is ${userPoint} against ${computerPoint}. Bad Game.`)
-  } else {
-    final.innerText = `Ties. Your score is ${userPoint} against to ${computerPoint}. Equality.`
-    console.log(`Ties. Your score is ${userPoint} against to ${computerPoint}. Equality.`)
-  }
-}
-
-
-game();
-
+// function game() {
+//   computerPoint = 0;
+//   userPoint = 0;
+//   // for (let game = 0; game < 5; game++) {
+//   const playerSelection = getPlayerChoice();
+//   const computerSelection = getComputerChoice();
+//   const roundResult = playRound(playerSelection, computerSelection);
+//   console.log(roundResult)
+// }
+// if (userPoint > computerPoint) {
+//   final.innerText = `You Win! Your score is ${userPoint} against ${computerPoint}. Congratulations.`
+//   console.log(`You Win! Your score is ${userPoint} against ${computerPoint}. Congratulations.`)
+// } else if (computerPoint > userPoint) {
+//   final.innerText = `You Lose! Your score is ${userPoint} against ${computerPoint}. Bad Game.`
+//   console.log(`You Lose! Your score is ${userPoint} against ${computerPoint}. Bad Game.`)
+// } else {
+//   final.innerText = `Ties. Your score is ${userPoint} against to ${computerPoint}. Equality.`
+//   console.log(`Ties. Your score is ${userPoint} against to ${computerPoint}. Equality.`)
+// }
 
 
 
