db38849ee624c31dbe09dcf1771678addcac1b78
Berkan K <berkan0hk@gmail.com>
2024-07-06 11:44:44 +0300

    sparkle effect add

--- script.js
+++ script.js
@@ -1,5 +1,75 @@
 console.log("Hello World!");
 
+// Sparkle Effect Code (Taken from https://codepen.io/kucsatax/pen/vyWevX)
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const colors = ["#FF0000", "#FF8C00", "#FFD700", "#ADFF2F", "#00FF00", "#00FA9A", "#00FFFF", "#1E90FF", "#0000FF", "#8A2BE2", "#FF00FF", "#FF1493"];
+
+class Sparkle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 10 + 5;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+    this.color = colors[Math.floor(Math.random() * colors.length)];
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.2;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+let sparkles = [];
+
+function initSparkles() {
+  for (let i = 0; i < 30; i++) {
+    const x = Math.random() * canvas.width; // Rastgele x koordinatı
+    const y = Math.random() * canvas.height; // Rastgele y koordinatı
+    sparkles.push(new Sparkle(x, y));
+  }
+}
+
+function handleSparkles() {
+  for (let i = 0; i < sparkles.length; i++) {
+    sparkles[i].update();
+    sparkles[i].draw();
+    if (sparkles[i].size <= 0.2) {
+      sparkles.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleSparkles();
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+
+// Sparkle Effect End
+console.log("Sparkle Effect Ready");
+
+
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
@@ -149,6 +219,7 @@ function FinalResults() {
     result.style.textShadow = '2px 2px 4px black';
     playerScore = 0;
     computerScore = 0;
+    initSparkles(); // Sparkle effect
   } else if (computerScore === 5) {
     result.innerText = `Yikes! You Lose the Game. Your Score is ${playerScore} against ${computerScore}.`;
     OutCome.append(result)
